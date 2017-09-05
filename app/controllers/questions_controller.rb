22f5b8612639b2834290e5afdabcc1aa00aaf412
Valinda Chan <valinda.chan@gmail.com>
2017-09-05 15:28:19 -0400

    Link titles, show question on details page, associate each book with an email

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -29,6 +29,7 @@ class QuestionsController < ApplicationController
   # POST /questions
   def create
     @question = Question.new(question_params)
+    @question.user = current_user
 
     if @question.save
       redirect_to @question, notice: 'Question was successfully created.'
